e39888bf2f9855768861458933a8f79cba27892d
Austin Williams <austinq.williams@gmail.com>
2019-02-04 19:56:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ delete newObj[key]
 }
 
  function destructivelyDeleteFromObjectByKey(object, key) {
-   const newObject = recipes.assign({}, recipes )
+   const newObject = Object.assign({}, newObject )
    return delete newObject[key]
  }
